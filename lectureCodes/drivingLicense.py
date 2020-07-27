198391d04530001afca695b90d4eab036348ec68
malakalsalman <malak96.salman@hotmail.com>
2020-07-27 23:04:49 +0300

    -

---
+++ lectureCodes/drivingLicense.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 17:55:47 2020
+
+@author: asf
+"""
+
+a = int(input("enter age "))
+g = input("enter gender ")
+if a>=18 and (g =="m" or g =="male") :
+    print("yes for male")
+#elif a<18 and (g =="m" or g =="male") :
+#    print("no for male")  
+elif a>=20 and (g =="f" or g =="fmale") :
+     print("yes for male")    
+else:
+    print("no d.l")
+     
+
