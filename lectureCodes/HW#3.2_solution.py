5fb0d612cdbce8f3a47da1911c87243d6a58e76b
malakalsalman <malak96.salman@hotmail.com>
2020-08-08 19:54:57 +0300

    -

---
+++ lectureCodes/HW#3.2_solution.py
@@ -0,0 +1,12 @@
+    
+for i in range(0,6):
+    for x in range(0,i):
+        print("*",end="")   
+    print("")
+    
+for i in range(0,6):
+    for j in range(5,i,-1):
+        print(" ",end="")
+    for x in range(0,i):
+        print("*",end="")   
+    print("")      
