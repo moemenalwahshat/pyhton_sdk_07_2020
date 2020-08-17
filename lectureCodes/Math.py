b0e6f5365c790aea4eec1db19139f78bf99c4d9c
malakalsalman <67827340+malakalsalman@users.noreply.github.com>
2020-08-17 18:19:35 +0300

    .

---
+++ lectureCodes/Math.py
@@ -0,0 +1,11 @@
+def Sum (a, b):
+    return a+b 
+
+def Sub(a, b):
+    return a-b 
+def mult (a, b):
+    return a*b 
+def power (a, b):
+    return a**b 
+x= 5 
+y= "welcome"
---
+++ lectureCodes/hw.py
@@ -0,0 +1,17 @@
+
+
+def swap(n1,n2):
+    
+    temp=n1
+    n1=n2
+    n2=temp
+    print("After swapping:","n1 =",n1,",","n2 =",n2)
+
+    
+
+
+
+
+
+
+
