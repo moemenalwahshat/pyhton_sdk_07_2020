26e7c3b3b787c020e4a964dc7f0655ed03271c91
malakalsalman <67827340+malakalsalman@users.noreply.github.com>
2020-08-19 18:25:08 +0300

    .

---
+++ lectureCodes/Ascii.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 18:07:05 2020
+
+@author: user2
+"""
+s1="hi" 
+s2 = "a"
+n = ord(s2)
+n-=32
+lettr = str(chr(n))
+# print(type(s1))
+print(lettr)
