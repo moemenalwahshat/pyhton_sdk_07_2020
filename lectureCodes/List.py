abca5803bcacf089b83f144dd60e41f55152ffc5
malakalsalman <67827340+malakalsalman@users.noreply.github.com>
2020-08-25 18:14:34 +0300

    Create List.py

---
+++ lectureCodes/List.py
@@ -0,0 +1,42 @@
+# numList = [1,2,3,4]
+# textList = ["rofidah " , "Moamem" ,"a","}"]
+# List = [ 1 ,4 ,'1',"hi "]
+# s = "12h"
+
+# print(numList +textList )
+# print(max(numList))
+# print(max(textList))
+# print(min(numList))
+# print(min(textList))
+# # print(max(List)) error
+
+# print(numList*2)
+# print(textList*4)
+# print(1 in numList )
+# print('1' in numList)
+# print('1' not in List)
+
+# List[0]= "welcome"
+# print(List)
+# print(len(List))
+# List[4]= 1 error
+# textList.append("abc")
+# textList.append(1)
+
+
+# textList.extend([1,2,3])
+# textList.extend(numList)
+# print(textList)
+# print(type(textList[len(textList)-1]))
+# textList.append(numList)
+# print(textList)
+list1 = ["abac", "dfge",'f']
+list2 = [ 1 , 2 , 3, 4 ,2 , 3 ,2]
+# print(list1.index("de"))
+print(list1[1].index('e'))
+print(list1.count('f'))
+print(list2.count(2))
+
+
+
+
