a681c64f12d836e3fc0627f09a930bf0d9bcb953
malakalsalman <malak96.salman@hotmail.com>
2020-09-21 20:45:13 +0300

    Update inheritance_cases.py

--- lectureCodes/oop/inheritance_cases.py
+++ lectureCodes/oop/inheritance_cases.py
@@ -4,7 +4,66 @@ Created on Sat Sep 19 16:54:45 2020
 
 @author: share
 """
+""" case one single inharitance """
+#class a :
+#    x=5
+#    def __init__(self):
+#        print("hii from a ")
+#    def info (self):
+#        print("hi")
+#class b(a) :
+#    def __init__(self):
+#        super().__init__()
+#        print("hii from b ")
+#class c :
+#    def __init__(self):
+#        super().__init__()
+#
+#        print("hii from c ")        
+#        
+#boj = b()
 
+""" case tow hirarchial inharitance """
+#class a :
+#    x=5
+#    def __init__(self):
+#        print("hii from a ")
+#    def info (self):
+#        print("hi")
+#class b(a) :
+#    def __init__(self):
+#        super().__init__()
+#        print("hii from b ")
+#class c(a) :
+#    def __init__(self):
+#        super().__init__()
+#
+#        print("hii from c ")        
+#        
+#boj1 = b()
+#obj2 = c()
+
+""" case three multiLevel inharitance """
+#class a :
+#    x=5
+#    def __init__(self):
+#        print("hii from a ")
+#    def info (self):
+#        print("hi")
+#class b(a) :
+#    def __init__(self):
+#        super().__init__()
+#        print("hii from b ")
+#class c(b) :
+#    def __init__(self):
+#        super().__init__()
+#
+#        print("hii from c ")        
+#        
+#boj1 = b()
+#obj2 = c()
+
+""" case one multiple inharitance """
 class a :
     x=5
     def __init__(self):
@@ -15,9 +74,11 @@ class b :
     def __init__(self):
         super().__init__()
         print("hii from b ")
-class c(b,a) :
+class c(a,b) :
     def __init__(self):
         super().__init__()
 
         print("hii from c ")        
         
+boj = c()
+
